3ccf3b9886671e49a0dbdd0e2b61629d8a6bf50c
xgill15x <jason_gill15@hotmail.com>
2024-12-02 14:12:12 -0800

    add tailwind to project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-center bg-blue-500 text-white py-10">
+      <h1 className="text-8xl font-bold">Hello Tailwind!</h1>
+    </div>
+  );
+}
+
+export default App;
